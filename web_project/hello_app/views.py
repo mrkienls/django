553faa93d54bf0bff6e92cee7e0447fcd0132735
mrkienls <mrkienls@gmail.com>
2019-11-12 08:17:31 +0700

    sao k load dc template

--- web_project/hello_app/views.py
+++ web_project/hello_app/views.py
@@ -7,20 +7,18 @@ def home(request):
     return HttpResponse("Hello, Django!")
 
 def hello_there(request, name):
-   return render(
-       request,
-       'hello_app/hello_there.html',
-       {
-           'name':name,
-           'date':datetime.now()
-       }
-   )
+    return render(
+        request,
+        'hello_app/hello_there.html',
+        {
+            'name':name,
+            'date':datetime.now()
+        }
+    )
 def home(request):
-    return render(request, "hello/home.html")
-
-def about(request):
-    return render(request, "hello/about.html")
-
+    return render(request, "hello_app/home.html")
 def contact(request):
-    return render(request, "hello/contact.html")
+    return render(request, "hello_app/contact.html")
+def about(request):
+    return render(request, "hello_app/about.html")    
 
